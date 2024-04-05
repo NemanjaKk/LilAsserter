d8b55cdcefddf5b647b02099667982a26089ad00
Nemanja Kukulicic <nemanja.kukulicic@vegait.rs>
2024-04-06 00:03:36 +0200

    Removed using

--- AsserterNemagus/Asserter.cs
+++ AsserterNemagus/Asserter.cs
@@ -1,5 +1,4 @@
-﻿using AsserterNemagus;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
