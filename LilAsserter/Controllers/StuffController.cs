2e7b2f9685177b1b78db78ff1dfbd2dfc7923508
Nemanja Kukulicic <Nemanja.Kukulicic@racfreedom.onmicrosoft.com>
2024-04-08 16:11:45 +0200

    Revert "Typo"
    This reverts commit e978f6f10ac5d5ee0eaa17aae1f2f99de5746afe.

--- LilAsserter/Controllers/StuffController.cs
+++ LilAsserter/Controllers/StuffController.cs
@@ -4,29 +4,38 @@ using Microsoft.AspNetCore.Mvc;
 namespace LilAsserter.Controllers;
 [ApiController]
 [Route("[controller]")]
-public class BlogService
+public class StuffController : ControllerBase
 {
     private readonly IAsserter _asserter;
-    private readonly IBlogRepository _blogRepository;
 
-    public BlogService(IAsserter asserter, IBlogRepository blogRepository)
+    public StuffController(IAsserter asserter)
     {
         _asserter = asserter;
-		_blogRepository = blogRepository;
     }
 
-	public List<Blog> GetBlogs(string searchTerm)
+    [HttpGet(Name = "GetStuff")]
+    public IActionResult Get()
     {
-		_asserter
-			.NotNull(searchTerm)
-			.Log(LogLevel.Critical, "Logging message")
-			.Assert();
-		// OR
-		_asserter
-			.NotNull(searchTerm)
-			.Log("Logging message", LogLevel.Critical)
-			.Assert();
+        _asserter
+            .TrueContinue(0 == 3)
+            .Message("1. Two numbers are not true.")
+            .Log("Logging Default that the two numbers are not true.")
+            .Assert();
 
-		return _blogRepository.GetBlogs(searchTerm);
+        _asserter
+            .TrueContinue(() =>
+            {
+                return 0 == 3;
+            })
+            .Message("2. Two numbers are not true.")
+            .Log("Logging Critical that the two numbers are not true.", LogLevel.Critical)
+            .Assert();
+
+        _asserter
+            .True(false)
+            .Log(LogLevel.Information, "Logging Information that the two numbers are not true.")
+            .Assert();
+
+        return Ok("Stuff");
     }
 }
